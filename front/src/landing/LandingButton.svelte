681abb8c3a89492b9eb21ed23d2297ad2e7f6fb2
Leinadium <daniel.sch.guima@gmail.com>
2023-04-27 12:04:20 -0300

    corrigindo cor novamente

--- front/src/landing/LandingButton.svelte
+++ front/src/landing/LandingButton.svelte
@@ -47,6 +47,7 @@
         border: 0px;
         box-sizing: border-box;
         cursor: pointer;
+        color: #fff;
 
         box-shadow: 0px 4px 16px rgba(17,17,26,0.1), 0px 8px 24px rgba(17,17,26,0.1), 0px 16px 56px rgba(17,17,26,0.1);
 
@@ -68,7 +69,6 @@
 
     .btn-continue {
         background-color: #0BBA48;
-        color: #fff;
     }
 
     .div-login {
