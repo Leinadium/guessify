f0df94be05b2143dabb51ab31e059ab1d1dafb9c
Leinadium <daniel.sch.guima@gmail.com>
2023-05-08 08:23:52 -0300

    testando gh-pages3

--- front/vite.config.js
+++ front/vite.config.js
@@ -3,8 +3,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  build: {
-    outDir: "./docs",
-  }
+  plugins: [svelte()]
 })
